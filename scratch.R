839c3c2b95ee36a149628869cdd58cb26384ae23
Jed Dougherty <jed.dougherty@dataiku.com>
2019-04-02 14:38:32 -0400

    better listing of quotes

--- scratch.R
+++ scratch.R
@@ -1,13 +1,13 @@
 # Lets talk about TAXES
 library(ggplot2)
-
+library(scales)
 
 # Generate function for piecewise splits
-splits    <- c(  0, 9525, 38700, 82500, 157500, 200000, 500000, 10000000)
-rates     <- c(0.1, 0.12,  0.22,  0.24,   0.32,   0.35,   0.37, 0.7)
+splits    <- c(  0, 9525, 38700, 82500, 157500, 200000, 500000)
+rates     <- c(0.1, 0.12,  0.22,  0.24,   0.32,   0.35,   0.37)
 rate_function <- stepfun(splits[-1], rates)
-max_income <- 2000000
-
+max_income <- 1000000
+max_rate   <- tail(rates,1)
 
 # Calculate actual taxes paid
 taxes <- function(x, ...){
@@ -46,13 +46,15 @@ v_take_home <- function(x, splits, rates){
 ggplot(data.frame(x=c(1,max_income)), aes(x)) +
   stat_function(fun=rate_function, geom="step", aes(colour="Marginal Rate"), n=1000, size = 0.5) +
   stat_function(fun=v_tax_rate, geom="path", aes(colour="Effective Rate"),   n=1000, size = 0.5) +
-  ylim(0,0.8) + 
+  scale_y_continuous(label=percent, limits = c(0,max_rate)) + 
+  scale_x_continuous(label=comma) + 
   xlab("Income") +
   ylab("Tax Rate")
 
 ggplot(data.frame(x=c(1,max_income)), aes(x)) +
   stat_function(fun=v_taxes_paid, geom="path", aes(colour="Total Taxes paid"),n=1000, size = 0.5) +
   stat_function(fun=v_take_home, geom="path", aes(colour="Take Home Pay"),    n=1000, size = 0.5) +
+  scale_y_continuous(label=comma, limits = c(0,max_income)) + 
+  scale_x_continuous(label=comma) + 
   xlab("Income") +
-  ylim(0, max_income) + 
   ylab("Dollars per Year")
